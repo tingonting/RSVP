d029fee32e9070d35ea04029d8c387af9096d40a
tingonting <118227714+tingonting@users.noreply.github.com>
2026-08-11 22:57:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ EVENT_END = datetime(2026, 11, 7, 23, 30)    # 11:30pm carriages
 
 DATA_COLUMNS = ["Guest 1", "Additional Guests", "Party Size", "Status", "Notes", "Timestamp"]
 
-HERO_IMAGE_PATH = os.path.join(os.path.dirname(__file__), "images", "hero1.jpg")
+HERO_IMAGE_PATH = os.path.join(os.path.dirname(__file__), "images", "IMG_0033.jpeg")
 
 
 def get_base64_image(path):
